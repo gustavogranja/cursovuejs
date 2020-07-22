a92e7cb71c63b3fccda53765226e5b76a4675313
gustavogranja <gustavo.cisco235@gmail.com>
2020-07-21 21:58:28 -0300

    Introducao aos componentes

---
+++ 6-componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 10" :key="c" />
+    </div>
+</template>
+
+<script>
+    import Contador from './Contador.vue'
+
+    export default {
+        components: { 'app-contador': Contador }
+    }
+</script>
+
+<style>
+
+</style>
