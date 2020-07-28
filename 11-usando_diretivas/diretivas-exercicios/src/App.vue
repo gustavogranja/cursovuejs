fd69e90ddb76a068f644c30e74f73c29abbb9888
gustavogranja <gustavo.cisco235@gmail.com>
2020-07-28 13:04:19 -0300

    Usando e criando diretivas

---
+++ 11-usando_diretivas/diretivas-exercicios/src/App.vue
@@ -0,0 +1,79 @@
+<! Gatilhos(HOOKS) :
+	bind(el, binding, vnode) = quando a diretiva é interligada com elemento;
+	inserted(el, binding, vnode) = elemento inserido no nó pai;
+	update(el, binding, vnode, oldVNode) = quando o componente é atualizado (antes filho);
+	componentUdated(el, binding, vnode, oldVNode) = quando o componente é atualizado (depois filho);
+	unbind(el, binding, vnode) = quando a diretiva é desvinculada do elemento
+		sua anatomia: v-nome:argumento.mod1.mod2="..."     ---->
+
+
+
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<hr>
+		<p v-destaque:fundo.atrasar="'blue'">Usando diretiva personalizada</p>
+		<p v-destaque.atrasar="cor">Usando diretiva personalizada</p>
+		<hr>
+		<p v-destaque-local:fundo.atrasar.alternar="{cor1: 'green', cor2: 'blue', atraso: 2000, intervalo: 200}">Usando diretiva personalizada</p>
+		<p v-destaque-local.atrasar="{cor1: 'green', atraso: 5000}">Usando diretiva personalizada</p>
+	</div>
+</template>
+
+<script>
+export default {
+	components: { },
+	directives: {
+		'destaque-local': {
+			bind(el, binding, vNode) {
+				const aplicarCor = cor => {
+					if (binding.arg === 'fundo') {
+						el.style.backgroundColor = cor
+					} else {
+						el.style.color = cor
+					}
+				}
+
+				let atraso = 0
+				if (binding.modifiers['atrasar']) atraso = binding.value.atraso
+
+				const cor1 = binding.value.cor1
+				const cor2 = binding.value.cor2
+				let corAtual = cor1
+
+				setTimeout(() => {
+					if (binding.modifiers['alternar']) {
+						setInterval(() => {
+							corAtual = corAtual === cor1 ? cor2 : cor1
+							aplicarCor(corAtual)
+						}, binding.value.intervalo)
+					} else  {
+						aplicarCor(binding.value.cor1)
+
+					}
+				}, atraso)
+			}
+		}
+	},
+	data() {
+		return {
+			cor: 'red'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
+
+
+
