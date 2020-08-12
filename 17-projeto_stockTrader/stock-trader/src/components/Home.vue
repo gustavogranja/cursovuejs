9e64384eb9573773309a675e55258b7a4966979b
gustavogranja <gustavo.cisco235@gmail.com>
2020-08-11 21:43:41 -0300

    Projeto - Stock Trader

---
+++ 17-projeto_stockTrader/stock-trader/src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1 class="display-3 font-weight-light mb-4">Negociar e Consulte suas Ações</h1>
+        <v-sheet :elevation="6" class="pa-2" color="primary">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">Voce pode Salvar & Carregar os Dados</span>
+        </v-sheet>
+        <v-sheet :elevation="6" class="pa-2 success mt-3">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Clique em 'Finalizar Dia' para inicar um novo dia!
+            </span>
+        </v-sheet>
+        <v-divider class="my-4" />
+        <p class="display-1"><strong>Seu Saldo:</strong> {{ funds }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            funds() {
+                return this.$store.getters.funds
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 17-projeto_stockTrader/stock-trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'G3T', price: 280 },
+    {id: 2, name: 'Google', price: 150 },
+    {id: 3, name: 'Apple', price: 201 },
+    {id: 4, name: 'Twitter', price: 99 },
+]
